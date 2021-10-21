83a8797e2a2087f8f3ee4c49f3cd7c38ca4583bc
Sincere <68159801+SEgbu@users.noreply.github.com>
2021-10-21 13:46:24 +0000

    Performed a bubble sort on the list

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import random
 
 # List of random integers
-unsorted = random.sample(range(0, 100), 10)
-print(unsorted)
+arr = random.sample(range(0, 10), 10)
+print("The random list: %s" %str(arr))
+
+# Bubble sort
+sorting = True
+
+while sorting == True:
+    for loopi in range(len(arr)):
+        for i in range(len(arr) - 1): # Iterate through unsorted list
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                continue
+            elif arr[i] <= arr[i + 1]:
+                continue
+
+    sorting = False
+
+print ("Sorted using bubble sort: %s" %str(arr))
+
