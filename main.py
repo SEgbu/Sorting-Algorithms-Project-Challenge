9a16a9297015a1d5a2a369e1ce71b69ed68920fd
Sincere <68159801+SEgbu@users.noreply.github.com>
2021-10-21 14:10:48 +0000

    Started insertion sort

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import random
 
 # List of random integers
-unsorted = random.sample(range(0, 100), 10)
-print(unsorted)
+arr = random.sample(range(0, 10), 10)
+print("The random list: %s" %str(arr))
+
+# Insertion sort
+sorted = [arr[0]] # The final sorted list
+sorting = True
+
+while sorting == True:
+    for i in arr:
+        if i == 0:
+            continue
+        if i > 0:
+            sorted.append(arr[i])
+            for sortedI in range(len(sorted)):
+                if sorted[sortedI] > sorted[sortedI - 1]:
+                    sorted[sortedI], sorted[sortedI - 1] = sorted[sortedI - 1], sorted[sortedI]
+                else: 
+                    continue
+
+
+
+print("Sorted using insertion sort: %s" %sorted)
+    
+
+
