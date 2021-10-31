00a0b9c8ed4f4cb60d7ad752e77e400304ba2397
Sincere <sincerityegbu@gmail.com>
2021-10-31 21:04:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ sorted = [arr[0]] # The final sorted list
 sorting = True
 
 while sorting == True:
-    for i in arr:
+    for i in range(len(arr)):
         if i == 0:
             continue
         if i > 0:
             sorted.append(arr[i])
-            for sortedI in range(len(sorted)):
-                if sorted[sortedI] > sorted[sortedI - 1]:
+            sortedI = len(sorted) - 1
+            while sortedI > 0:
+                if sorted[sortedI] < sorted[sortedI - 1]:
                     sorted[sortedI], sorted[sortedI - 1] = sorted[sortedI - 1], sorted[sortedI]
+                    sortedI -= 1
                 else: 
+                    sortedI -= 1
                     continue
-
+    sorting = False
 
 
 print("Sorted using insertion sort: %s" %sorted)
